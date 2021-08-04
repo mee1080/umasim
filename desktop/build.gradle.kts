aca9bec903b0269afc15a57bf9a0e2cabb55ed9a
mee1080 <bzote304@yahoo.co.jp>
2021-08-04 16:24:24 +0900

    GUIを別モジュールに切り出し

---
+++ desktop/build.gradle.kts
@@ -0,0 +1,36 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm")
+    id("org.jetbrains.compose") version "0.4.0"
+}
+
+group = "io.github.mee1080.umasim"
+version = "1.0"
+
+repositories {
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+}
+
+dependencies {
+    implementation(project(":core"))
+    implementation(project(":jvm"))
+    implementation(compose.desktop.currentOs)
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "16"
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "umasim"
+            packageVersion = "1.0.0"
+        }
+    }
+}
