29c24b02aa3b5cad5c30bf039dbef4a8e6b3e502
mee1080 <bzote304@yahoo.co.jp>
2021-09-29 12:00:26 +0900

    commit 2021/09/29 12:00:26.42

--- jvm/src/main/kotlin/io/github/mee1080/umasim/cui/cui.kt
+++ jvm/src/main/kotlin/io/github/mee1080/umasim/cui/cui.kt
@@ -184,23 +184,23 @@ fun openCui(args: Array<String>) {
 //    }
 
     // 長距離スピスタ
-//    optimizeAI(
-//        Store.getChara("ゴールドシップ", 5, 5), Store.getSupportByName(
-//            *(speed(4, 3)),
-//            *(stamina(4, 3)),
-//        ), options = generateOptions(
-//            step = 0.1,
-//            speed = 1.0..1.2,
-//            stamina = 1.0..1.2,
-//            power = 0.4..0.6,
-//            guts = 0.4..0.6,
-//            hp = 0.6..0.8,
-//        ), testCount = 1000
-//    )
-//    doLongSimulation(
-//        StatusType.STAMINA, 0..4, 4,
-//        100000, option = FactorBasedActionSelector.speedStamina
-//    )
+    optimizeAI(
+        Store.getChara("ゴールドシップ", 5, 5), Store.getSupportByName(
+            *(speed(4, 3)),
+            *(stamina(4, 3)),
+        ), options = generateOptions(
+            step = 0.1,
+            speed = 1.0..1.2,
+            stamina = 1.0..1.2,
+            power = 0.4..0.6,
+            guts = 0.4..0.6,
+            hp = 0.6..0.8,
+        ), testCount = 1000
+    )
+    doLongSimulation(
+        StatusType.SPEED, 0..4, 4,
+        100000, option = FactorBasedActionSelector.speedStamina
+    )
 
     // マイルパワ賢
 //    optimizeAI(
@@ -249,7 +249,7 @@ fun openCui(args: Array<String>) {
 //    )
 //    doCharmSimulation()
 //    doFailureRateSimulation()
-    checkNewSimulator()
+//    checkNewSimulator()
     context.close()
 }
 
