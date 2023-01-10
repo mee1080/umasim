defc7e1feb662c6549e7b2a0490f1ea686605a10
mee1080 <bzote304@yahoo.co.jp>
2023-01-10 12:11:26 +0900

    commit 2023/01/10 12:11:26.35

--- jvm/src/main/kotlin/io/github/mee1080/umasim/cui/cui.kt
+++ jvm/src/main/kotlin/io/github/mee1080/umasim/cui/cui.kt
@@ -658,9 +658,9 @@ fun openCui(args: Array<String>) {
 //            "[駆けよ、駆けよ、駆けよ！！]オグリキャップ" to 4,
 //            "[from the GROUND UP]ライトハロー" to 4,
 //        ).toTypedArray(),
-//        targetStatus = StatusType.WISDOM, rarity = 2..3, talent = 0..4,
-////        Store.getSupportByName(*((0..4).map { "[Are you merry?]エアグルーヴ" to it }.toTypedArray())),
-////        Store.getSupportByName("[Are you merry?]エアグルーヴ" to 4),
+////        targetStatus = StatusType.WISDOM, rarity = 2..3, talent = 0..4,
+////        Store.getSupportByName(*((0..4).map { "[気まぐれ絶佳]スイープトウショウ" to it }.toTypedArray())),
+//        Store.getSupportByName("[気まぐれ絶佳]スイープトウショウ" to 4),
 //        factor = factor(StatusType.GUTS, 4) + factor(StatusType.STAMINA, 2),
 //        testCount = 10000,
 //        option = GrandLiveFactorBasedActionSelector.speed3Wisdom2Friend1,
@@ -686,42 +686,42 @@ fun openCui(args: Array<String>) {
 //    )
 
     // スピ3スタ1賢さ1ハロー
-//    doSimulation2(
-//        Scenario.GRAND_LIVE,
-//        Store.getChara("[プラタナス・ウィッチ]スイープトウショウ", 5, 5),
-//        Store.getSupportByName(
-//            "[迫る熱に押されて]キタサンブラック" to 4,
-//            "[Q≠0]アグネスタキオン" to 4,
-//            "[おセンチ注意報♪]マルゼンスキー" to 4,
-//            "[Dear Mr. C.B.]ミスターシービー" to 4,
-//            "[from the GROUND UP]ライトハロー" to 4,
-//        ).toTypedArray(),
-//        targetStatus = StatusType.STAMINA, rarity = 2..3, talent = 0..4,
-////        Store.getSupportByName(*((0..4).map { "[一粒の安らぎ]スーパークリーク" to it }.toTypedArray())),
-////        Store.getSupportByName("[一粒の安らぎ]スーパークリーク" to 4),
-//        factor = factor(StatusType.STAMINA, 6),
-//        testCount = 10000,
-//        option = GrandLiveFactorBasedActionSelector.speed3Stamina1Wisdom1Friend1,
-//        evaluateSetting = Runner.grandLiveLongEvaluateSetting,
-//    )
-
-    // スピ2スタ2賢さ1ハロー
     doSimulation2(
         Scenario.GRAND_LIVE,
         Store.getChara("[プラタナス・ウィッチ]スイープトウショウ", 5, 5),
         Store.getSupportByName(
             "[迫る熱に押されて]キタサンブラック" to 4,
             "[Q≠0]アグネスタキオン" to 4,
-            "[A Win Foreshadowed]シンボリクリスエス" to 4,
+            "[おセンチ注意報♪]マルゼンスキー" to 4,
             "[Dear Mr. C.B.]ミスターシービー" to 4,
             "[from the GROUND UP]ライトハロー" to 4,
         ).toTypedArray(),
 //        targetStatus = StatusType.STAMINA, rarity = 2..3, talent = 0..4,
-//        Store.getSupportByName(*((0..4).map { "[一粒の安らぎ]スーパークリーク" to it }.toTypedArray())),
-        Store.getSupportByName("[一粒の安らぎ]スーパークリーク" to 4),
-        factor = factor(StatusType.STAMINA, 3) + factor(StatusType.WISDOM, 3),
+        Store.getSupportByName(*((0..4).map { "[ハネ退け魔を退け願い込め]スペシャルウィーク" to it }.toTypedArray())),
+//        Store.getSupportByName("[一粒の安らぎ]スーパークリーク" to 4),
+        factor = factor(StatusType.STAMINA, 6),
         testCount = 10000,
-        option = GrandLiveFactorBasedActionSelector.speed2Stamina2Wisdom1Friend1,
+        option = GrandLiveFactorBasedActionSelector.speed3Stamina1Wisdom1Friend1,
         evaluateSetting = Runner.grandLiveLongEvaluateSetting,
     )
+
+    // スピ2スタ2賢さ1ハロー
+//    doSimulation2(
+//        Scenario.GRAND_LIVE,
+//        Store.getChara("[プラタナス・ウィッチ]スイープトウショウ", 5, 5),
+//        Store.getSupportByName(
+//            "[迫る熱に押されて]キタサンブラック" to 4,
+//            "[Q≠0]アグネスタキオン" to 4,
+//            "[A Win Foreshadowed]シンボリクリスエス" to 4,
+//            "[Dear Mr. C.B.]ミスターシービー" to 4,
+//            "[from the GROUND UP]ライトハロー" to 4,
+//        ).toTypedArray(),
+////        targetStatus = StatusType.STAMINA, rarity = 2..3, talent = 0..4,
+////        Store.getSupportByName(*((0..4).map { "[一粒の安らぎ]スーパークリーク" to it }.toTypedArray())),
+//        Store.getSupportByName("[一粒の安らぎ]スーパークリーク" to 4),
+//        factor = factor(StatusType.STAMINA, 3) + factor(StatusType.WISDOM, 3),
+//        testCount = 10000,
+//        option = GrandLiveFactorBasedActionSelector.speed2Stamina2Wisdom1Friend1,
+//        evaluateSetting = Runner.grandLiveLongEvaluateSetting,
+//    )
 }
