2ccb9c96e889e6bb8cf602b72e9c29e7e332cf23
mee1080 <bzote304@yahoo.co.jp>
2024-01-31 11:55:59 +0900

    commit 2024/01/31 11:55:59.87

--- jvm/src/main/kotlin/io/github/mee1080/umasim/cui/simulateLArc.kt
+++ jvm/src/main/kotlin/io/github/mee1080/umasim/cui/simulateLArc.kt
@@ -13,44 +13,44 @@ fun simulateLarc() {
 }
 
 private fun speed3Stamina1Wisdom1Long() {
-//    doSimulation2(
-//        Scenario.LARC,
-//        Store.getChara("[うららん一等賞♪]ハルウララ", 5, 5),
-//        Store.getSupportByName(
-//            "[大望は飛んでいく]エルコンドルパサー",
-//            "[The frontier]ジャングルポケット",
-////            "[迫る熱に押されて]キタサンブラック",
-//            "[ハネ退け魔を退け願い込め]スペシャルウィーク",
-//            "[君と見る泡沫]マンハッタンカフェ",
-//            "[L'aubeは迫りて]佐岳メイ",
-//        ).toTypedArray(),
-////        targetStatus = StatusType.SPEED, rarity = 2..3, talent = 0..4,
-//        Store.getSupportByName(*((0..4).map { "[血脈の胎動]ドゥラメンテ" to it }.toTypedArray())),
-////        Store.getSupportByName("[おセンチ注意報♪]マルゼンスキー" to 4),
-//        factor = factor(StatusType.STAMINA, 6),
-//        testCount = 100000,
-//        selector = LArcActionSelector.speed3Stamina1Wisdom1Long,
-//        evaluateSetting = Runner.lArcLongEvaluateSetting,
-//    )
     doSimulation2(
         Scenario.LARC,
         Store.getChara("[うららん一等賞♪]ハルウララ", 5, 5),
         Store.getSupportByName(
             "[大望は飛んでいく]エルコンドルパサー",
             "[The frontier]ジャングルポケット",
-            "[迫る熱に押されて]キタサンブラック",
-//            "[一粒の安らぎ]スーパークリーク",
+//            "[迫る熱に押されて]キタサンブラック",
+            "[ハネ退け魔を退け願い込め]スペシャルウィーク",
             "[君と見る泡沫]マンハッタンカフェ",
             "[L'aubeは迫りて]佐岳メイ",
         ).toTypedArray(),
-//        targetStatus = StatusType.STAMINA, rarity = 2..3, talent = 0..4,
-        Store.getSupportByName(*((0..4).map { "[白い翼は舞い戻りて]ホッコータルマエ" to it }.toTypedArray())),
+//        targetStatus = StatusType.SPEED, rarity = 2..3, talent = 0..4,
+        Store.getSupportByName(*((0..4).map { "[感謝感謝！サクラ吹雪！！]サクラバクシンオー" to it }.toTypedArray())),
 //        Store.getSupportByName("[おセンチ注意報♪]マルゼンスキー" to 4),
         factor = factor(StatusType.STAMINA, 6),
         testCount = 100000,
         selector = LArcActionSelector.speed3Stamina1Wisdom1Long,
         evaluateSetting = Runner.lArcLongEvaluateSetting,
     )
+//    doSimulation2(
+//        Scenario.LARC,
+//        Store.getChara("[うららん一等賞♪]ハルウララ", 5, 5),
+//        Store.getSupportByName(
+//            "[大望は飛んでいく]エルコンドルパサー",
+//            "[The frontier]ジャングルポケット",
+//            "[迫る熱に押されて]キタサンブラック",
+////            "[一粒の安らぎ]スーパークリーク",
+//            "[君と見る泡沫]マンハッタンカフェ",
+//            "[L'aubeは迫りて]佐岳メイ",
+//        ).toTypedArray(),
+////        targetStatus = StatusType.STAMINA, rarity = 2..3, talent = 0..4,
+//        Store.getSupportByName(*((0..4).map { "[白い翼は舞い戻りて]ホッコータルマエ" to it }.toTypedArray())),
+////        Store.getSupportByName("[おセンチ注意報♪]マルゼンスキー" to 4),
+//        factor = factor(StatusType.STAMINA, 6),
+//        testCount = 100000,
+//        selector = LArcActionSelector.speed3Stamina1Wisdom1Long,
+//        evaluateSetting = Runner.lArcLongEvaluateSetting,
+//    )
 }
 
 private fun speed3Power1Wisdom1Middle() {
@@ -76,25 +76,6 @@ private fun speed3Power1Wisdom1Middle() {
 }
 
 private fun speed2Guts2Wisdom1Mile() {
-//    doSimulation2(
-//        Scenario.LARC,
-//        Store.getChara("[超特急！フルカラー特殊PP]アグネスデジタル", 5, 5),
-//        Store.getSupportByName(
-//            "[大望は飛んでいく]エルコンドルパサー",
-//            "[The frontier]ジャングルポケット",
-//            "[うらら～な休日]ハルウララ",
-////            "[優しい月]ゴールドシチー",
-//            "[燦爛]メジロラモーヌ",
-//            "[L'aubeは迫りて]佐岳メイ",
-//        ).toTypedArray(),
-//        targetStatus = StatusType.GUTS, rarity = 2..3, talent = 0..4,
-////        Store.getSupportByName(*((0..4).map { "[我が舞、巡りて]キングヘイロー" to it }.toTypedArray())),
-////        Store.getSupportByName("[ロード・オブ・ウオッカ]ウオッカ" to 4),
-//        factor = factor(StatusType.SPEED, 2) + factor(StatusType.STAMINA, 2) + factor(StatusType.POWER, 2),
-//        testCount = 100000,
-//        selector = LArcActionSelector.speed2Guts2Wisdom1Mile,
-//        evaluateSetting = Runner.lArcMileEvaluateSetting,
-//    )
     doSimulation2(
         Scenario.LARC,
         Store.getChara("[超特急！フルカラー特殊PP]アグネスデジタル", 5, 5),
@@ -102,16 +83,35 @@ private fun speed2Guts2Wisdom1Mile() {
             "[大望は飛んでいく]エルコンドルパサー",
             "[The frontier]ジャングルポケット",
             "[うらら～な休日]ハルウララ",
-            "[優しい月]ゴールドシチー",
-//            "[燦爛]メジロラモーヌ",
+//            "[優しい月]ゴールドシチー",
+            "[燦爛]メジロラモーヌ",
             "[L'aubeは迫りて]佐岳メイ",
         ).toTypedArray(),
-        targetStatus = StatusType.WISDOM, rarity = 2..3, talent = 0..4,
-//        Store.getSupportByName(*((0..4).map { "[我が舞、巡りて]キングヘイロー" to it }.toTypedArray())),
+        targetStatus = StatusType.GUTS, rarity = 2..3, talent = 0..4,
+//        Store.getSupportByName(*((0..4).map { "[2人のバウンス・シャッセ]キタサンブラック" to it }.toTypedArray())),
 //        Store.getSupportByName("[ロード・オブ・ウオッカ]ウオッカ" to 4),
         factor = factor(StatusType.SPEED, 2) + factor(StatusType.STAMINA, 2) + factor(StatusType.POWER, 2),
         testCount = 100000,
         selector = LArcActionSelector.speed2Guts2Wisdom1Mile,
         evaluateSetting = Runner.lArcMileEvaluateSetting,
     )
+//    doSimulation2(
+//        Scenario.LARC,
+//        Store.getChara("[超特急！フルカラー特殊PP]アグネスデジタル", 5, 5),
+//        Store.getSupportByName(
+//            "[大望は飛んでいく]エルコンドルパサー",
+//            "[The frontier]ジャングルポケット",
+//            "[うらら～な休日]ハルウララ",
+//            "[優しい月]ゴールドシチー",
+////            "[燦爛]メジロラモーヌ",
+//            "[L'aubeは迫りて]佐岳メイ",
+//        ).toTypedArray(),
+//        targetStatus = StatusType.WISDOM, rarity = 2..3, talent = 0..4,
+////        Store.getSupportByName(*((0..4).map { "[我が舞、巡りて]キングヘイロー" to it }.toTypedArray())),
+////        Store.getSupportByName("[ロード・オブ・ウオッカ]ウオッカ" to 4),
+//        factor = factor(StatusType.SPEED, 2) + factor(StatusType.STAMINA, 2) + factor(StatusType.POWER, 2),
+//        testCount = 100000,
+//        selector = LArcActionSelector.speed2Guts2Wisdom1Mile,
+//        evaluateSetting = Runner.lArcMileEvaluateSetting,
+//    )
 }
