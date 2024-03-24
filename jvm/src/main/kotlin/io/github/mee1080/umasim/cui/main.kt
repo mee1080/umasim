05ae445cae3a49bc1de46707ddb8b757dc6b2a8a
mee1080 <bzote304@yahoo.co.jp>
2024-03-24 09:20:57 +0900

    UAFシミュレーション設定

--- jvm/src/main/kotlin/io/github/mee1080/umasim/cui/main.kt
+++ jvm/src/main/kotlin/io/github/mee1080/umasim/cui/main.kt
@@ -23,7 +23,7 @@ import kotlinx.coroutines.ExecutorCoroutineDispatcher
 import kotlinx.coroutines.asCoroutineDispatcher
 import java.util.concurrent.Executors
 
-const val THREAD_COUNT = 8
+const val THREAD_COUNT = 5
 lateinit var context: ExecutorCoroutineDispatcher
 
 fun main(args: Array<String>) {
---
+++ jvm/src/main/kotlin/io/github/mee1080/umasim/cui/simulateUaf.kt
@@ -0,0 +1,33 @@
+package io.github.mee1080.umasim.cui
+
+import io.github.mee1080.umasim.ai.UafActionSelector
+import io.github.mee1080.umasim.data.Scenario
+import io.github.mee1080.umasim.data.StatusType
+import io.github.mee1080.umasim.data.Store
+import io.github.mee1080.umasim.simulation2.Runner
+
+fun simulateUaf() {
+    speed2Power1Guts1Wisdom1Mile()
+}
+
+private fun speed2Power1Guts1Wisdom1Mile() {
+    doSimulation2(
+        Scenario.UAF,
+        Store.getChara("[プラタナス・ウィッチ]スイープトウショウ", 5, 5),
+        Store.getSupportByName(
+//            "[血脈の胎動]ドゥラメンテ",
+            "[大望は飛んでいく]エルコンドルパサー",
+            "[冬溶かす熾火]メジロラモーヌ",
+            "[只、君臨す。]オルフェーヴル",
+            "[かっとばせー！ですわ！？]メジロマックイーン",
+            "[共に描くキラメキ]都留岐涼花",
+        ).toTypedArray(),
+        targetStatus = StatusType.SPEED, rarity = 2..3, talent = 0..4,
+//        Store.getSupportByName(*((0..4).map { "[感謝感謝！サクラ吹雪！！]サクラバクシンオー" to it }.toTypedArray())),
+//        Store.getSupportByName("[おセンチ注意報♪]マルゼンスキー" to 4),
+        factor = factor(StatusType.SPEED, 1) + factor(StatusType.STAMINA, 1) + factor(StatusType.POWER, 4),
+        testCount = 100000,
+        selector = UafActionSelector.speed2Power1Guts1Wisdom1Mile,
+        evaluateSetting = Runner.uafMileEvaluateSetting,
+    )
+}
