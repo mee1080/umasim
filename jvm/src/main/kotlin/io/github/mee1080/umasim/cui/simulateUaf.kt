31ace5345a6dd489b6e99bea48153f1a7d0cc0ba
mee1080 <bzote304@yahoo.co.jp>
2024-05-03 20:28:09 +0900

    シミュレーション設定

--- jvm/src/main/kotlin/io/github/mee1080/umasim/cui/simulateUaf.kt
+++ jvm/src/main/kotlin/io/github/mee1080/umasim/cui/simulateUaf.kt
@@ -7,8 +7,8 @@ import io.github.mee1080.umasim.data.Store
 import io.github.mee1080.umasim.simulation2.Runner
 
 fun simulateUaf() {
-//    speed2Power1Guts1Wisdom1Mile()
-    speed2Stamina1Guts1Wisdom1Long()
+    speed2Power1Guts1Wisdom1Mile()
+//    speed2Stamina1Guts1Wisdom1Long()
 }
 
 private fun speed2Power1Guts1Wisdom1Mile() {
@@ -50,25 +50,25 @@ private fun speed2Power1Guts1Wisdom1Mile() {
 //        selector = UafActionSelector.speed2Power1Guts1Wisdom1Mile,
 //        evaluateSetting = Runner.uafMileEvaluateSetting,
 //    )
-//    doSimulation2(
-//        Scenario.UAF,
-//        Store.getChara("[プラタナス・ウィッチ]スイープトウショウ", 5, 5),
-//        Store.getSupportByName(
-//            "[血脈の胎動]ドゥラメンテ",
-//            "[大望は飛んでいく]エルコンドルパサー",
-//            "[冬溶かす熾火]メジロラモーヌ",
-////            "[只、君臨す。]オルフェーヴル",
-//            "[かっとばせー！ですわ！？]メジロマックイーン",
-//            "[共に描くキラメキ]都留岐涼花",
-//        ).toTypedArray(),
-//        targetStatus = StatusType.GUTS, rarity = 3..3, talent = 0..3,
-////        Store.getSupportByName(*((0..4).map { "[感謝感謝！サクラ吹雪！！]サクラバクシンオー" to it }.toTypedArray())),
-////        Store.getSupportByName("[おセンチ注意報♪]マルゼンスキー" to 4),
-//        factor = factor(StatusType.SPEED, 1) + factor(StatusType.STAMINA, 1) + factor(StatusType.POWER, 4),
-//        testCount = 100000,
-//        selector = UafActionSelector.speed2Power1Guts1Wisdom1Mile,
-//        evaluateSetting = Runner.uafMileEvaluateSetting,
-//    )
+    doSimulation2(
+        Scenario.UAF,
+        Store.getChara("[プラタナス・ウィッチ]スイープトウショウ", 5, 5),
+        Store.getSupportByName(
+            "[血脈の胎動]ドゥラメンテ",
+            "[大望は飛んでいく]エルコンドルパサー",
+            "[冬溶かす熾火]メジロラモーヌ",
+//            "[只、君臨す。]オルフェーヴル",
+            "[かっとばせー！ですわ！？]メジロマックイーン",
+            "[共に描くキラメキ]都留岐涼花",
+        ).toTypedArray(),
+        targetStatus = StatusType.GUTS, rarity = 2..2, talent = 0..3,
+//        Store.getSupportByName(*((0..4).map { "[感謝感謝！サクラ吹雪！！]サクラバクシンオー" to it }.toTypedArray())),
+//        Store.getSupportByName("[おセンチ注意報♪]マルゼンスキー" to 4),
+        factor = factor(StatusType.SPEED, 1) + factor(StatusType.STAMINA, 1) + factor(StatusType.POWER, 4),
+        testCount = 100000,
+        selector = UafActionSelector.speed2Power1Guts1Wisdom1Mile,
+        evaluateSetting = Runner.uafMileEvaluateSetting,
+    )
 //    doSimulation2(
 //        Scenario.UAF,
 //        Store.getChara("[プラタナス・ウィッチ]スイープトウショウ", 5, 5),
