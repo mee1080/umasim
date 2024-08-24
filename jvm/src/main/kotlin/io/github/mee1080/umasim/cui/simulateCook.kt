483e0803ec9856c39237fac6d1495608f9680fbd
mee1080 <bzote304@yahoo.co.jp>
2024-08-24 14:12:04 +0900

    シミュレーション設定更新

--- jvm/src/main/kotlin/io/github/mee1080/umasim/cui/simulateCook.kt
+++ jvm/src/main/kotlin/io/github/mee1080/umasim/cui/simulateCook.kt
@@ -12,10 +12,10 @@ fun simulateCook() {
 
 private fun speed1Power1Guts2Wisdom1Short() {
     var defaultSupport = Store.getSupportByName(
-        "[アルストロメリアの夢]ヴィブロス",
+//        "[アルストロメリアの夢]ヴィブロス",
         "[朝焼け苺の畑にて]ニシノフラワー",
         "[うらら～な休日]ハルウララ",
-//        "[只、君臨す。]オルフェーヴル",
+        "[只、君臨す。]オルフェーヴル",
         "[Take Them Down!]ナリタタイシン",
         "[謹製ッ！特大夢にんじん！]秋川理事長",
     ).toTypedArray()
@@ -25,8 +25,8 @@ private fun speed1Power1Guts2Wisdom1Short() {
         Store.getChara("[リアライズ・ルーン]スイープトウショウ", 5, 5),
         defaultSupport,
 //        targetStatus, rarity = 2..3, talent = 0..4,
-        Store.getSupportByName(*((0..4).map { "[歴史も美食も余すところなく]ファインモーション" to it }.toTypedArray())),
-//        Store.getSupportByName("[おセンチ注意報♪]マルゼンスキー" to 4),
+        Store.getSupportByName(*((0..4).map { "[Devilish Whispers]スティルインラブ" to it }.toTypedArray())),
+//        Store.getSupportByName("[アルストロメリアの夢]ヴィブロス" to 4),
         factor = factor(StatusType.SPEED, 6),
         testCount = 100000,
         selector = CookActionSelector.speed1Power1Guts2Wisdom1Short,
