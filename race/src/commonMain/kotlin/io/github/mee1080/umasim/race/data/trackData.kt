f3115ae3030de08513962707ca42aa40a9a5025b
mee1080 <bzote304@yahoo.co.jp>
2024-09-27 21:14:15 +0900

    LoH条件追加

--- race/src/commonMain/kotlin/io/github/mee1080/umasim/race/data/trackData.kt
+++ race/src/commonMain/kotlin/io/github/mee1080/umasim/race/data/trackData.kt
@@ -45,6 +45,11 @@ val recentEventTrackList by lazy {
         RecentEventTrackEntry("京都", "芝1600m(外)", CourseCondition.BAD, 12),
 
         RecentEventTrackEntry("東京", "芝2000", CourseCondition.GOOD, 9),
+
+        RecentEventTrackEntry("京都", "芝2200m(外)", CourseCondition.GOOD, 12),
+        RecentEventTrackEntry("京都", "芝2200m(外)", CourseCondition.YAYAOMO, 12),
+        RecentEventTrackEntry("京都", "芝2200m(外)", CourseCondition.OMO, 12),
+        RecentEventTrackEntry("京都", "芝2200m(外)", CourseCondition.BAD, 12),
     ).mapNotNull { target ->
         val course = trackData.entries.firstOrNull { it.value.name == target.courseName } ?: return@mapNotNull null
         val track = course.value.courses.entries.firstOrNull { it.value.name.startsWith(target.distance) }
