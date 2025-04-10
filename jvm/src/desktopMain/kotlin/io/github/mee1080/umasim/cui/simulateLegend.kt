37ac97356d5823023f0cc4fbd4e750ea57265a3e
mee1080 <bzote304@yahoo.co.jp>
2025-04-10 12:17:06 +0900

    シミュレーション実行

--- jvm/src/desktopMain/kotlin/io/github/mee1080/umasim/cui/simulateLegend.kt
+++ jvm/src/desktopMain/kotlin/io/github/mee1080/umasim/cui/simulateLegend.kt
@@ -10,9 +10,9 @@ import io.github.mee1080.umasim.scenario.legend.getLegendBuff
 import io.github.mee1080.umasim.simulation2.Runner
 
 fun simulateLegend() {
-    speed2Stamina2Wisdom1Speed()
+//    speed2Stamina2Wisdom1Speed()
 //    speed2Stamina2Wisdom1Stamina()
-//    speed2Stamina2Wisdom1Wisdom()
+    speed2Stamina2Wisdom1Wisdom()
 }
 
 private fun speed2Stamina2Wisdom1Speed() {
@@ -103,8 +103,8 @@ private fun speed2Stamina2Wisdom1Wisdom() {
         scenario,
         chara,
         defaultSupport,
-        targetStatus, rarity = 2..3, talent = 0..4,
-//        Store.getSupportByName(*((0..4).map { "[世界を変える眼差し]アーモンドアイ" to it }.toTypedArray())),
+//        targetStatus, rarity = 2..3, talent = 0..4,
+        Store.getSupportByName(*((0..4).map { "[無垢の白妙]デアリングタクト" to it }.toTypedArray())),
 //        Store.getSupportByName("[大望は飛んでいく]エルコンドルパサー" to 4),
         factor = factor(StatusType.SPEED, 2) + factor(StatusType.POWER, 1) + factor(StatusType.STAMINA, 3),
         testCount = 100000,
