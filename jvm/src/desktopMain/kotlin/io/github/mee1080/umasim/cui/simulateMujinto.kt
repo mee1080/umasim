02e3fc139b70fecfbd5e834f4d40d2a445cc8e79
mee1080 <bzote304@yahoo.co.jp>
2025-07-31 22:10:15 +0900

    シミュレーション結果追加

--- jvm/src/desktopMain/kotlin/io/github/mee1080/umasim/cui/simulateMujinto.kt
+++ jvm/src/desktopMain/kotlin/io/github/mee1080/umasim/cui/simulateMujinto.kt
@@ -7,7 +7,9 @@ import io.github.mee1080.umasim.scenario.Scenario
 import io.github.mee1080.umasim.simulation2.Runner
 
 fun simulateMujinto() {
-    speed1Stamina1power1guts1Wisdom1Speed()
+//    speed1Stamina1power1guts1Wisdom1Speed()
+//    speed1Stamina1power1guts1Wisdom1Wisdom()
+    speed1Stamina1power1guts1Wisdom1Stamina()
 }
 
 private fun speed1Stamina1power1guts1Wisdom1Speed() {
@@ -22,6 +24,60 @@ private fun speed1Stamina1power1guts1Wisdom1Speed() {
         "[本能は吼えているか！？]タッカーブライン" to 4,
     ).toTypedArray()
     val targetStatus = StatusType.SPEED
+    doSimulation2(
+        scenario,
+        chara,
+        defaultSupport,
+        targetStatus, rarity = 2..3, talent = 0..4,
+//        Store.getSupportByName(*((0..4).map { "[世界を変える眼差し]アーモンドアイ" to it }.toTypedArray())),
+//        Store.getSupportByName("[大望は飛んでいく]エルコンドルパサー" to 4),
+        factor = factor(StatusType.SPEED, 6),
+        testCount = 100000,
+        selector = MujintoActionSelector.defaultOption::generateSelector,
+        evaluateSetting = Runner.mujintoSetting,
+        evaluateUpperRate = 0.2,
+    )
+}
+
+private fun speed1Stamina1power1guts1Wisdom1Stamina() {
+    val scenario = Scenario.MUJINTO
+    val chara = Store.getChara("[リアライズ・ルーン]スイープトウショウ", 5, 5)
+    val defaultSupport = Store.getSupportByName(
+        "[世界を変える眼差し]アーモンドアイ" to 4,
+//        "[Cocoon]エアシャカール" to 4,
+        "[白き稲妻の如く]タマモクロス" to 4,
+        "[只、君臨す。]オルフェーヴル" to 4,
+        "[Take Them Down!]ナリタタイシン" to 4,
+        "[本能は吼えているか！？]タッカーブライン" to 4,
+    ).toTypedArray()
+    val targetStatus = StatusType.STAMINA
+    doSimulation2(
+        scenario,
+        chara,
+        defaultSupport,
+        targetStatus, rarity = 2..3, talent = 0..4,
+//        Store.getSupportByName(*((0..4).map { "[世界を変える眼差し]アーモンドアイ" to it }.toTypedArray())),
+//        Store.getSupportByName("[大望は飛んでいく]エルコンドルパサー" to 4),
+        factor = factor(StatusType.SPEED, 6),
+        testCount = 100000,
+        selector = MujintoActionSelector.defaultOption::generateSelector,
+        evaluateSetting = Runner.mujintoSetting,
+        evaluateUpperRate = 0.2,
+    )
+}
+
+private fun speed1Stamina1power1guts1Wisdom1Wisdom() {
+    val scenario = Scenario.MUJINTO
+    val chara = Store.getChara("[リアライズ・ルーン]スイープトウショウ", 5, 5)
+    val defaultSupport = Store.getSupportByName(
+        "[世界を変える眼差し]アーモンドアイ" to 4,
+        "[Cocoon]エアシャカール" to 4,
+        "[白き稲妻の如く]タマモクロス" to 4,
+        "[只、君臨す。]オルフェーヴル" to 4,
+//        "[Take Them Down!]ナリタタイシン" to 4,
+        "[本能は吼えているか！？]タッカーブライン" to 4,
+    ).toTypedArray()
+    val targetStatus = StatusType.WISDOM
     doSimulation2(
         scenario,
         chara,
