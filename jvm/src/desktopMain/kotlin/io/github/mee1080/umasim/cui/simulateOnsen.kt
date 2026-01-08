9f7943b70591a0e1c30f76b81719d22f770d5cfb
mee1080 <bzote304@yahoo.co.jp>
2026-01-08 11:39:00 +0900

    commit 2026/01/08 11:39:00.79

--- jvm/src/desktopMain/kotlin/io/github/mee1080/umasim/cui/simulateOnsen.kt
+++ jvm/src/desktopMain/kotlin/io/github/mee1080/umasim/cui/simulateOnsen.kt
@@ -14,8 +14,8 @@ import io.github.mee1080.utility.applyIf
 fun simulateOnsen() {
 //    speed2power1guts1Wisdom1Speed()
 //    speed2stamina1power1Wisdom1Stamina()
-    speed2power1guts1Wisdom1Power()
-//    speed2power1guts1Wisdom1Guts()
+//    speed2power1guts1Wisdom1Power()
+    speed2power1guts1Wisdom1Guts()
 //    speed2power1guts1Wisdom1Wisdom()
 //    speed2power1guts1Wisdom1Friend()
 }
